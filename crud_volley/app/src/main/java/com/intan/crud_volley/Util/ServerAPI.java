4b4cf516c21c680545537403cb39eafaa35f646e
IntanKusumaning <intankusumaningtribuana@gmail.com>
2021-06-09 21:29:35 +0700

    menambah tugas

---
+++ crud_volley/app/src/main/java/com/intan/crud_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.intan.crud_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.1.47/pemrograman_web_smt4/Gol-C_E31192033_Intan-Kusumaning-Tribuana_WEB/minggu13/ci/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.1.47/pemrograman_web_smt4/Gol-C_E31192033_Intan-Kusumaning-Tribuana_WEB/minggu13/ci/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.1.47/pemrograman_web_smt4/Gol-C_E31192033_Intan-Kusumaning-Tribuana_WEB/minggu13/ci/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.1.47/pemrograman_web_smt4/Gol-C_E31192033_Intan-Kusumaning-Tribuana_WEB/minggu13/ci/Mahasiswa/ApiUpdate";
+}
